7966ddde2b5a4e39f3e7b518c39fb0be1b188de5
sebax <sebaxtian.tc@gmail.com>
2022-09-28 20:00:22 -0500

    Initial Commit

---
+++ TetGAPSI/TetGAPSIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TetGAPSIApp.swift
+//  TetGAPSI
+//
+//  Created by Sebastian TC on 28/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TetGAPSIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
